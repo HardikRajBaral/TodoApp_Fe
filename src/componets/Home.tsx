9410c092bdb69c9dd40b646f9e57938c0cbfc8be
HardikRajBaral <hardikrajbaral232@gmail.com>
2025-08-12 21:04:54 +0545

    Starting to make React Project for TodoApp to learn react

---
+++ src/componets/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home= ()=>{
+    return(
+        <div>
+            <h1>Welcome to the Todo App</h1>
+            <p>This is the home page.</p>
+        </div>
+    )
+}
+
+export default Home
